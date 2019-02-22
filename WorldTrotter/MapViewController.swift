505b27e0dd33eba51d8953223f6c7f67e03c7901
matthew <matthew@matthew.com>
2019-02-22 10:44:36 -0600

    Silver Challenge.

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     override func loadView() {
         mapView = MKMapView()
@@ -52,12 +55,88 @@ class MapViewController: UIViewController {
     }
     
     func zoomToCurrentLocation() {
+        // Based on Shobhakar Tiwari's code here:
+        // stackoverflow.com/questions/41189147/mapkit-zoom-to-user-current-location
+        print("about to zoom.")
+        locationManager.startUpdatingLocation()
+        print("startUpdatingLocation called.")
+        //startReceivingLocationChanges()
+        if let currentLocation = locationManager.location {
+            print("got latitude: \(String(describing: locationManager.location?.coordinate.latitude))")
+            let currentCoord = currentLocation.coordinate
+
+            let currentLocationViewRegion = MKCoordinateRegion.init(center: currentCoord, latitudinalMeters: 50000, longitudinalMeters: 50000)
+            mapView.setRegion(currentLocationViewRegion, animated: true)
+            print("zoomed.")
+        } else {
+            print("locationManager.location is NIL")
+        }
         
+    // This next line was shutting down locationManager PERMANENTLY;
+    // The call to "startUpdatingLocation()" at the begining of this func
+    // would then have no effect, and "locationManager.location" would always be nil
+        //locationManager.stopUpdatingLocation()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Map loaded.")
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            print("authorized")
+        } else {
+            print("NOT authorized")
+        }
+    }
+    
+    func startReceivingLocationChanges() {
+        // This func is basically taken from the Apple documentation.
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse {
+            print("startReceiving... NOT authorized")
+            return
+        }
+        if !CLLocationManager.locationServicesEnabled() {
+            print("location services NOT enabled")
+            return
+        }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        // probably the most critical line of this function.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // This code is from:
+        // cs-courses.mines.edu/csci498b_fall2016/lectures/14/CoreLocationMapKitNotes.pdf
+        let clError = error as! CLError
+        var errorType = String()
+        
+        switch clError.code {
+        case .denied:
+            errorType = "Location service denied."
+        case .locationUnknown:
+            errorType = "Location can't be determined."
+        default:
+            errorType = error.localizedDescription
+        }
+        
+        let alert = UIAlertController(title: "Location access error", message: errorType, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
 }
